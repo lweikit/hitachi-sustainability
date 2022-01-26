b573e42506658101a4f107150880355d5da3a244
lweikit <al.weikit@gmail.com>
2022-01-26 22:51:38 +0800

    styled components

--- components/CaseCard.tsx
+++ components/CaseCard.tsx
@@ -1,24 +1,53 @@
+import styled from 'styled-components';
 import styles from '../styles/components/CaseCard.module.scss'
 
-export default function CaseCard({ number, description, href, img }) {
+interface Props { 
+  number?: string, 
+  description: string, 
+  href: string, 
+  img: string 
+}
+
+export default function CaseCard({
+  number,
+  description, 
+  href, 
+  img
+}: Props) {
   return (
-    <div className={styles.container}>
-      <div className={styles.image}>
-        <img src={img}/>
-      </div>
-      <div className={styles.cover}>
-        { number }
-      </div>
-      <div className={styles.overlay}>
-        <div className={styles.description}>
-          { description }
-        </div>
-        <div>
-          <a href={href} target="_blank">
-            Learn More 
-          </a>
-        </div>
-      </div>
-    </div>
+    // <CaseCardStyled>
+      <CaseCardStyled 
+        href={href}
+        target="_blank">
+        <ContentWrap background={img}>
+          <Description>
+            {description}
+          </Description>
+        </ContentWrap>
+      </CaseCardStyled>
   )
 }
+
+const CaseCardStyled = styled.a`
+  display: flex;
+  width: 100%;
+  height: 300px;
+`;
+
+const ContentWrap = styled.div<{ background: string }>`
+  position: relative;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background-color: utils.$c_green_99;
+  background-image: ${({ background }) => `url(${background})`};
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: 0.5s ease-out;
+`;
+
+const Description = styled.span`
+  position: absolute;
+  bottom: 30px;
+
+`;
--- sections/11.tsx
+++ sections/11.tsx
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import styles from "../styles/sections/11.module.scss"
 
 import { Row, Col } from "reactstrap"
@@ -5,37 +6,94 @@ import CaseCard from "../components/CaseCard"
 
 const S11 = () => {
   return (
-    <div className={styles.container}>
-      <Row>
-        <Col xs={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
-          <h3>
-            Want to find out more?
-          </h3>
-          <CaseCard 
-            number={'01'}
-            description={'Vehicle-sharing Service in Thailand'}
-            href={'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/'}
-            img={'sections/11/vehicle-sharing-TH.png'}
+    // <div className={styles.container}>
+    //   <Row>
+    //     <Col xs={{ size: 10, offset: 1 }} md={{ size: 6, offset: 3 }}>
+    //       <h3>
+    //         Want to find out more?
+    //       </h3>
+    //       <CaseCard 
+    //         description="Vehicle-sharing Service in Thailand"
+    //         href={'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/'}
+    //         img={'sections/11/vehicle-sharing-TH.png'}
+    //       />
+    //       <CaseCard 
+    //         description={'Battery Energy Storage System in the Philippines'}
+    //         href={'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/'}
+    //         img={'sections/11/battery-energy-storage-PH.png'}
+    //       />
+    //       <CaseCard 
+    //         number={'03'}
+    //         description={'Big Data Analytics in Singapore'}
+    //         href={'https://social-innovation.hitachi/en-sg/innovationhub/businessinnovations/'}
+    //         img={'sections/11/big-data-analytics-SG.png'}
+    //       />
+    //       <a href="./references/references.pdf">
+    //         References
+    //       </a>
+    //     </Col>
+    //   </Row>
+    // </div>
+    <LayoutStyled>
+      <Title>Want to find out more?</Title>
+      <CardWrap>
+        {cardsConfig.map((card, index) => (
+          <CaseCard  
+            key={`case-card-${index}`}
+            description={card.description}
+            href={card.href} 
+            img={card.img}  
           />
-          <CaseCard 
-            number={'02'}
-            description={'Battery Energy Storage System in the Philippines'}
-            href={'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/'}
-            img={'sections/11/battery-energy-storage-PH.png'}
-          />
-          <CaseCard 
-            number={'03'}
-            description={'Big Data Analytics in Singapore'}
-            href={'https://social-innovation.hitachi/en-sg/innovationhub/businessinnovations/'}
-            img={'sections/11/big-data-analytics-SG.png'}
-          />
-          <a href="./references/references.pdf">
-            References
-          </a>
-        </Col>
-      </Row>
-    </div>
+        ))}
+      </CardWrap>
+
+
+    </LayoutStyled>
   )
 }
+
+const cardsConfig = [
+  {
+    description: 'Vehicle-sharing Service in Thailand',
+    href: 'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/',
+    img: 'sections/11/vehicle-sharing-TH.png'
+  },
+  {
+    description: 'Vehicle-sharing Service in Thailand',
+    href: 'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/',
+    img: 'sections/11/vehicle-sharing-TH.png'
+  },
+  {
+    description: 'Vehicle-sharing Service in Thailand',
+    href: 'https://www.eqmagpro.com/meralco-hitachi-set-up-2-mw-battery-energy-storage-system/',
+    img: 'sections/11/vehicle-sharing-TH.png'
+  }
+];
+
+const LayoutStyled = styled.div`
+  display: flex;
+  width: 100%;
+  padding: 40px 20px;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Title = styled.h3`
+  
+`
+
+const CardWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 480px;
+  row-gap: 20px;
+
+  @media only screen and (max-width: 480px) {
+    width: 100% !important;
+  }
+`
 
 export default S11
